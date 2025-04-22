64fcfee9d3473321dcb3baa352bf6e5b108c554c
Nicholas Hamilton <nicholashamilton@Mac.localdomain>
2025-04-22 18:29:33 +1000

    chore: point at real TOKEN_DEPLOY_BLOCK & TOKEN_ADDRESS

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 // src/constants.ts
 
 export const TOKEN_DEPLOY_BLOCK = 29259918;
-export const TOKEN_ADDRESS      = "0x5a34646B86040x3c5dc85dcf8c48ad44130f10571d27724475b522";
+export const TOKEN_ADDRESS      = "0x3c5dc85dcf8c48ad44130f10571d27724475b522";
 export const MAX_TRANSACTIONS   = 10000;
 export const BATCH_SIZE         = 2000;
 export const BATCH_DELAY        = 1000;
